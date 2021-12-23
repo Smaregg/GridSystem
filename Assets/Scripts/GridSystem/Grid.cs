04bb081b4ac2d5d12c9358c736677366a464e203
周周 <wuzht@yoozoo.com>
2021-12-23 18:43:58 +0800

    GridSystem

---
+++ Assets/Scripts/GridSystem/Grid.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GridSystem
+{
+    public class Grid
+    {
+        public Vector3 Position { get { return m_v3Position; } }
+
+        public List<Vector3[]> GetAllEdges()
+        {
+            List<Vector3[]> allEdges = new List<Vector3[]>();
+            for (int i = 0; i < m_iWidth; i++)
+                for (int j = 0; j < m_iHeight; j++)
+                    allEdges.Add(GetCellEdge(i, j));
+            return allEdges;
+        }
+
+        public Vector3[] GetCellEdge(int indexX, int indexY)
+        {
+            Vector3 origin = GetCellWorldPosition(indexX, indexY);
+            Vector3[] edges = new Vector3[3];
+            edges[0] = origin;
+            edges[1] = new Vector3(origin.x + m_iCellSize, origin.y, origin.z);
+            edges[2] = new Vector3(origin.x, origin.y, origin.z + m_iCellSize);
+            return edges;
+        }
+
+        public Vector3 GetCellWorldPosition(int indexX, int indexY)
+        {
+            return new Vector3(m_v3Position.x + indexX * m_iCellSize, m_v3Position.y, m_v3Position.z + indexY * m_iCellSize);
+        }
+
+        public Grid(Vector3 position, int width, int height, int cellSize)
+        {
+            m_v3Position = position;
+            m_iWidth = width;
+            m_iHeight = height;
+            m_iCellSize = cellSize;
+        }
+
+        private Vector3 m_v3Position = Vector3.zero;
+
+        private int m_iWidth = 0;
+
+        private int m_iHeight = 0;
+
+        private int m_iCellSize = 0;
+    }
+}
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GridSystem;
+
+public class TestScript : MonoBehaviour
+{
+    public Vector3 GridPosition = Vector3.zero;
+
+    public int GridX = 0;
+
+    public int GridY = 0;
+
+    public int CellSize = 0;
+
+    #region 生命周期
+    private void Start()
+    {
+        m_grid = new GridSystem.Grid(GridPosition, GridX, GridY, CellSize);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 rightUp = new Vector3(GridPosition.x + GridX * CellSize, GridPosition.y, GridPosition.z);
+        Vector3 leftDown = new Vector3(GridPosition.x, GridPosition.y, GridPosition.z + GridY * CellSize);
+        Vector3 rightDown = new Vector3(GridPosition.x + GridX * CellSize, GridPosition.y, GridPosition.z + GridY * CellSize);
+        Gizmos.DrawLine(GridPosition, rightUp);
+        Gizmos.DrawLine(GridPosition, leftDown);
+        Gizmos.DrawLine(rightUp, rightDown);
+        Gizmos.DrawLine(leftDown, rightDown);
+
+        if (m_grid != null)
+        {
+            List<Vector3[]> gridEdges = m_grid.GetAllEdges();
+            Vector3 origin = Vector3.zero;
+            Vector3 originRight = Vector3.zero;
+            Vector3 originDown = Vector3.zero;
+            for (int i = 0; i < gridEdges.Count; i++)
+            {
+                origin = gridEdges[i][0];
+                originRight = gridEdges[i][1];
+                originDown = gridEdges[i][2];
+                Gizmos.DrawLine(origin, originRight);
+                Gizmos.DrawLine(origin, originDown);
+            }
+        }
+    }
+    #endregion
+
+    private GridSystem.Grid m_grid = null;
+}
